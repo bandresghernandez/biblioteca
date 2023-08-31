e685b1c57f9fee3e8dd380d89b3c472522407ef0
bandresghernandez <48714822+bandresghernandez@users.noreply.github.com>
2023-08-31 09:56:33 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            text-align: center;
+        }
+
+        .container {
+            margin-top: 100px;
+            width: 300px;
+            background-color: #fff;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            padding: 20px;
+            display: inline-block;
+        }
+
+        h2 {
+            margin-bottom: 20px;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 10px;
+        }
+
+        input[type="text"],
+        input[type="password"] {
+            width: 100%;
+            padding: 5px;
+            margin-bottom: 10px;
+            border: 1px solid #ccc;
+            border-radius: 3px;
+        }
+
+        input[type="submit"] {
+            background-color: #4CAF50;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 3px;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+	      <h2>Sistema de Biblioteca</h2>
+        <h3>Iniciar sesión</h3>
+
+        <form action="controller/login.php" method="POST">
+            <label for="username">Usuario:</label>
+            <input type="text" id="username" name="username" required><br>
+
+            <label for="password">Contraseña:</label>
+            <input type="password" id="password" name="password" required><br>
+
+            <input type="submit" value="Iniciar sesión">
+        </form>
+    </div>
+</body>
+</html>
